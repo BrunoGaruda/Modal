d7f4420897a54af7098aa8d6c019e8336236b194
BrunoGaruda <bruleorn@gmail.com>
2022-06-27 14:30:10 -0300

    adicionando o classList para remover a hidden class

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-'use strict';
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+console.log(btnsOpenModal)
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked')
+
+    // Neste caso a classe nao adiciona o ponto "." e sim o somente o nome da classe.
+    // Caso removermos o hidden o texto aparecerá
+    modal.classList.remove('hidden')
+  })
