87215fa7c318f4fb32e65c37162d99ce23fa2667
BrunoGaruda <bruleorn@gmail.com>
2022-07-02 16:27:55 -0300

    adição do botão esc para sair do modal

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
-console.log(btnsOpenModal)
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked')
-
-    // Neste caso a classe nao adiciona o ponto "." e sim o somente o nome da classe.
-    // Caso removermos o hidden o texto aparecerá
-    modal.classList.remove('hidden')
-
-    //backdrop-filter blur
-    overlay.classList.remove('hidden')
-  })
-
-btnCloseModal.addEventListener('click', function () {
+const closeModel = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
+}
+
+const openModel = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  // Ativando o model removendo a class hidden com a função
+  btnsOpenModal[i].addEventListener('click', openModel)
+
+// Fechamento no botão com a função
+btnCloseModal.addEventListener('click', closeModel)
+
+// Fechamento clicando fora do model (overlay) com a função
+overlay.addEventListener('click', closeModel)
+
+//Global event ao clicar no "esc" (escape)
+// Parametro "e" para função ser executada quando a tecla for pressionada
+document.addEventListener('keydown', function (e) {
+  //e.key será o botão clicado
+  // console.log(e.key)
+  // Escape tem que ser com letra maiúscula
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel()
+  }
 })
